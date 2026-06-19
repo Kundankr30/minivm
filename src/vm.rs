e6ace1158bf0a790eed8dbe7063f8712bf459818
Kundankr30 <kundanixr@gmail.com>
2026-06-19 10:23:34 +0530

    feat: execute EQ, LT, GT, JMP, JZ, JNZ and trap handling

--- src/vm.rs
+++ src/vm.rs
@@ -1,9 +1,8 @@
-use std::{convert::TryInto};
 use crate::isa;
+use std::convert::TryInto;
 
 pub fn run(file: &str, trace: bool) -> Result<(), String> {
-    let bytes = std::fs::read(file)
-        .map_err(|e| format!("could not read file: {}", e))?;
+    let bytes = std::fs::read(file).map_err(|e| format!("could not read file: {}", e))?;
     if &bytes[0..4] != &[0x4D, 0x56, 0x4D, 0x00] {
         return Err("Invalid tbc file".to_string());
     }
@@ -13,7 +12,7 @@ pub fn run(file: &str, trace: bool) -> Result<(), String> {
     let code_len = u32::from_le_bytes(
         bytes[5..9]
             .try_into()
-            .map_err(|_| "Invalid code length".to_string())?
+            .map_err(|_| "Invalid code length".to_string())?,
     ) as usize;
     let code = &bytes[9..9 + code_len];
     println!("File valid! code Length: {} bytes", code_len);
@@ -28,15 +27,18 @@ pub fn run(file: &str, trace: bool) -> Result<(), String> {
         ip = new_ip;
         match op {
             isa::Op::Push(n) => {
-                if stack.len()>=1024{
-                    return Err(format!("trap at ip=0x{:04X}:stack overflow (stack full)",ip));
+                if stack.len() >= 1024 {
+                    return Err(format!(
+                        "trap at ip=0x{:04X}:stack overflow (stack full)",
+                        ip
+                    ));
                 }
                 stack.push(n);
             }
             isa::Op::Pop => {
                 //tack.pop();
                 match stack.pop() {
-                    Some(_) => {},
+                    Some(_) => {}
                     None => return Err(format!("trap at ip=0x{:04X}: stack underflow", ip)),
                 }
             }
@@ -45,65 +47,93 @@ pub fn run(file: &str, trace: bool) -> Result<(), String> {
                 stack.push(top);
             }
             isa::Op::Swap => {
-                let a = safe_pop(&mut stack,ip)?;
-                let b = safe_pop(&mut stack,ip)?;
+                let a = safe_pop(&mut stack, ip)?;
+                let b = safe_pop(&mut stack, ip)?;
                 stack.push(a);
                 stack.push(b);
             }
             isa::Op::Add => {
-                let b = safe_pop(&mut stack,ip)?;
-                let a = safe_pop(&mut stack,ip)?;
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
                 stack.push(a + b);
             }
             isa::Op::Sub => {
-                let b = safe_pop(&mut stack,ip)?;
-                let a = safe_pop(&mut stack,ip)?;
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
                 stack.push(a - b);
             }
             isa::Op::Mul => {
-                let b = safe_pop(&mut stack,ip)?;
-                let a = safe_pop(&mut stack,ip)?;
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
                 stack.push(a * b);
             }
             isa::Op::Div => {
-                let b = safe_pop(&mut stack,ip)?;
-                let a = safe_pop(&mut stack,ip)?;
-                if b==0{
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
+                if b == 0 {
                     return Err(format!("trap at ip=0x{:04X}: division by zero", ip));
                 }
                 stack.push(a / b);
             }
             isa::Op::Mod => {
-                let b = safe_pop(&mut stack,ip)?;
-                let a = safe_pop(&mut stack,ip)?;
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
                 if b == 0 {
                     return Err(format!("trap at ip=0x{:04X}: modulo by zero", ip));
                 }
                 stack.push(a % b);
             }
             isa::Op::Neg => {
-                let a = safe_pop(&mut stack,ip)?;
+                let a = safe_pop(&mut stack, ip)?;
                 stack.push(-a);
             }
             isa::Op::Load(s) => {
                 stack.push(globals[s as usize]);
             }
             isa::Op::Store(s) => {
-                let a = safe_pop(&mut stack,ip)?;
+                let a = safe_pop(&mut stack, ip)?;
                 globals[s as usize] = a;
             }
             isa::Op::Print => {
-                let ans = safe_pop(&mut stack,ip)?;
+                let ans = safe_pop(&mut stack, ip)?;
                 println!("{}", ans);
             }
             isa::Op::Halt => break,
+            isa::Op::EQ => {
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
+                stack.push(if a == b { 1 } else { 0 });
+            }
+            isa::Op::LT => {
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
+                stack.push(if a < b { 1 } else { 0 });
+            }
+            isa::Op::GT => {
+                let b = safe_pop(&mut stack, ip)?;
+                let a = safe_pop(&mut stack, ip)?;
+                stack.push(if a > b { 1 } else { 0 });
+            }
+            isa::Op::JMP(addr) => {
+                ip = addr as usize;
+            }
+            isa::Op::JZ(addr) => {
+                if safe_pop(&mut stack, ip)? == 0 {
+                    ip = addr as usize;
+                }
+            }
+            isa::Op::JNZ(addr) => {
+                if safe_pop(&mut stack, ip)? != 0 {
+                    ip = addr as usize;
+                }
+            }
         }
     }
     Ok(())
 }
- fn safe_pop(stack:&mut Vec<i64>,ip:usize) ->Result<i64,String>{
-        match stack.pop(){
-            Some(v) =>Ok(v),
-            None => return Err(format!("trap at ip=0x{:04X}:stack underflow",ip)),
-        }
+fn safe_pop(stack: &mut Vec<i64>, ip: usize) -> Result<i64, String> {
+    match stack.pop() {
+        Some(v) => Ok(v),
+        None => return Err(format!("trap at ip=0x{:04X}:stack underflow", ip)),
+    }
 }
