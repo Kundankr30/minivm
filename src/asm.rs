bbf4fd847c1a90ee79461771e0963ef1cfb28105
Kundankr30 <kundanixr@gmail.com>
2026-06-19 10:23:34 +0530

    feat: parse EQ, LT, GT mnemonics in assembler

--- src/asm.rs
+++ src/asm.rs
@@ -1,12 +1,16 @@
-use std::fs;
 use crate::isa;
-pub fn assemble(file:&str)->Result<(),String>{
+use std::fs;
+pub fn assemble(file: &str) -> Result<(), String> {
     let contents = fs::read_to_string(file).expect("Count not real file");
     let mut code: Vec<u8> = Vec::new();
-    for line in contents.lines(){
+    for line in contents.lines() {
         let line = line.trim();
-        if line.is_empty(){continue;}
-        if line.starts_with(';'){continue;}
+        if line.is_empty() {
+            continue;
+        }
+        if line.starts_with(';') {
+            continue;
+        }
         //let line = match line.split_once(';'){
         //    Some((code,_))=>code.trim(),
         //     None=>line,
@@ -14,10 +18,10 @@ pub fn assemble(file:&str)->Result<(),String>{
         //handling comment lines;
         let op = parse_line(line);
         code.extend_from_slice(&isa::encode(op));
-        println!("{:?}",op);
+        println!("{:?}", op);
     }
-    let mut output :Vec<u8>= Vec::new();
-    output.extend_from_slice(&[0x4D,0x56,0x4D,0x00]);
+    let mut output: Vec<u8> = Vec::new();
+    output.extend_from_slice(&[0x4D, 0x56, 0x4D, 0x00]);
     output.push(0x01);
     output.extend_from_slice(&(code.len() as u32).to_le_bytes());
     output.extend_from_slice(&code);
@@ -25,27 +29,26 @@ pub fn assemble(file:&str)->Result<(),String>{
     fs::write(&out_file, &output).expect("could not write file");
     println!("written to {}", out_file);
     Ok(())
-
 }
-fn parse_line(line:&str) ->crate::isa::Op{
+fn parse_line(line: &str) -> crate::isa::Op {
     let mut parts = line.split_whitespace();
     let memonics = parts.next().unwrap().to_uppercase();
-    match memonics.as_str(){
-        "PUSH" =>{
-            // push "7" ->some("push") some("7" ) --->unwarap "7"-->unwrap we get 7 
+    match memonics.as_str() {
+        "PUSH" => {
+            // push "7" ->some("push") some("7" ) --->unwarap "7"-->unwrap we get 7
             let n = parts.next().unwrap().parse().unwrap();
             crate::isa::Op::Push(n)
         }
-                "POP"   => crate::isa::Op::Pop,
-        "DUP"   => crate::isa::Op::Dup,
-        "SWAP"  => crate::isa::Op::Swap,
-        "ADD"   => crate::isa::Op::Add,
-        "SUB"   => crate::isa::Op::Sub,
-        "MUL"   => crate::isa::Op::Mul,
-        "DIV"   => crate::isa::Op::Div,
-        "MOD"   => crate::isa::Op::Mod,
-        "NEG"   => crate::isa::Op::Neg,
-        "LOAD"  => {
+        "POP" => crate::isa::Op::Pop,
+        "DUP" => crate::isa::Op::Dup,
+        "SWAP" => crate::isa::Op::Swap,
+        "ADD" => crate::isa::Op::Add,
+        "SUB" => crate::isa::Op::Sub,
+        "MUL" => crate::isa::Op::Mul,
+        "DIV" => crate::isa::Op::Div,
+        "MOD" => crate::isa::Op::Mod,
+        "NEG" => crate::isa::Op::Neg,
+        "LOAD" => {
             let s: u8 = parts.next().unwrap().parse().unwrap();
             crate::isa::Op::Load(s)
         }
@@ -54,7 +57,10 @@ fn parse_line(line:&str) ->crate::isa::Op{
             crate::isa::Op::Store(s)
         }
         "PRINT" => crate::isa::Op::Print,
-        "HALT"  => crate::isa::Op::Halt,
-        other   => panic!("unknown mnemonic: {}", other)
+        "HALT" => crate::isa::Op::Halt,
+        "EQ" => crate::isa::Op::EQ,
+        "LT" => crate::isa::Op::LT,
+        "GT" => crate::isa::Op::GT,
+        other => panic!("unknown mnemonic: {}", other),
     }
-}   
+}
